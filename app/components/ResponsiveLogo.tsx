36c4a85b5d487ceeb4d37a8a233f79163b757692
Bradley Anderson <brad.anderson2718@gmail.com>
2024-11-20 15:28:31 -0500

    Removes remix boilerplate, roughs out header

---
+++ app/components/ResponsiveLogo.tsx
@@ -0,0 +1,15 @@
+
+export const ResponsiveLogo = () => (
+  <>
+    <img
+      src="https://search.censys.io/static/img/censys-2022.svg"
+      alt="Censys"
+      className="w-[150px] hidden md:block"
+    />
+    <img
+      src="https://search.censys.io/static/img/censys-mark-2022.svg"
+      alt="Censys"
+      className="w-[50px] md:hidden"
+    />
+  </>
+);
